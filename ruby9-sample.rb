1dccbc91df923bd11b036815b73c36a037903f76
愿山聖 <asjstksmr@gmail.com>
2020-06-16 14:30:43 +0900

    [fin] ruby９章

---
+++ ruby9-sample.rb
@@ -0,0 +1,28 @@
+def hello_world
+	'Hello, World'
+end
+
+puts hello_world
+
+
+
+def greeting(name)
+	"Hello, #{name}"
+end
+
+puts greeting('John')
+
+
+def greeting
+	"Hello, John"
+	"Hi, John"
+end
+puts greeting
+
+
+puts 1==2
+puts 1==1
+
+
+puts 8 % 3
+puts 5%5
